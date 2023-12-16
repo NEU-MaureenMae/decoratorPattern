9361fec98342a4e29bb68419b97947a4b1edd880
stud-mauu <maureen.sebastian@neu.edu.ph>
2023-12-17 07:42:24 +0800

    Introduce GSave class

---
+++ decorator/src/GSave.java
@@ -0,0 +1,38 @@
+class GSave implements BankAccountDecorator, BankAccount {
+    private BankAccount bankAccount;
+
+    @Override
+    public void setBankAccount(BankAccount account) {
+        this.bankAccount = account;
+    }
+
+    @Override
+    public String showAccountType() {
+        return "GSave";
+    }
+
+    @Override
+    public double getInterestRate() {
+        return 0.025; // 2.5%
+    }
+
+    @Override
+    public double getBalance() {
+        return bankAccount.getBalance();
+    }
+
+    @Override
+    public String showBenefits() {
+        return bankAccount.showBenefits() + ", GCash Transfer";
+    }
+
+    @Override
+    public double computeBalanceWithInterest() {
+        return bankAccount.getBalance() * (1 + getInterestRate());
+    }
+
+    @Override
+    public String showInfo() {
+        return bankAccount.showInfo();
+    }
+}
